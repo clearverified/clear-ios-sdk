c7df97b9b625e3850542ab4eb6867548812de791
Phil Rattazzi <79532397+philrattazzi-clear@users.noreply.github.com>
2021-10-29 17:44:21 -0400

    IDV-558: [Task] Add support for swift package manager (#1)

--- Package.swift
+++ Package.swift
@@ -8,9 +8,13 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
+        .library(name: "ClearMeSDK", targets: ["TrustKit", "ClearFaceCapture", "ClearMeSDK"])
     ],
     dependencies: [
     ],
     targets: [
+        .binaryTarget( name: "TrustKit", url: "https://github.com/unrestrictedidentity/clear-mobile-sdk-ios-public/releases/download/0.0.0/TrustKit.xcframework.zip", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+        .binaryTarget(name: "ClearFaceCapture", url: "https://github.com/unrestrictedidentity/clear-mobile-sdk-ios-public/releases/download/0.0.0/ClearFaceCapture.xcframework.zip", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+        .binaryTarget(name: "ClearMeSDK", url: "https://github.com/unrestrictedidentity/clear-mobile-sdk-ios-public/releases/download/0.0.0/ClearMeSDK.xcframework.zip", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
     ]
 )
