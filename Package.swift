761047a7646f3084e2ebddadea52ded9b9058ddd
runner <runner@ip-10-192-76-4.ec2.internal>
2023-09-13 15:14:49 +0000

    [Version] 1.2.0

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "Clear", url: "https://github.com/clearsecureidentity/clear-ios-sdk/releases/download/1.1.0/Clear.xcframework.zip", checksum: "9ae87754ae9ded6424d8fb979676b29285f799ea7d1d5df38bc68d805d3922ab"),
+        .binaryTarget(name: "Clear", url: "https://github.com/clearsecureidentity/clear-ios-sdk/releases/download/1.2.0/Clear.xcframework.zip", checksum: "33dd8267ebaef76b28d3e206c556018334bca8dc18482222c4863272b5668d10"),
     ]
 )
